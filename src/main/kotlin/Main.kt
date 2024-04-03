32e0283c3e68d98c4173c321f3b9ef514db24152
log8450 <log8450@mobility42.io>
2024-04-03 10:01:09 +0900

    ..

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,39 +3,26 @@ const val MODULO = 42
 // https://dmoj.ca/problems/?search=COCI+%2706+Contest+1
 
 fun main(args: Array<String>) {
-    val patternP1 = "..#.".toCharArray()
-    val patternP2 = ".#.#".toCharArray()
-    val str = readln()
+    val missionNumber = readln().toInt()
 
-    if(str.length > 15) throw Throwable("Invalid Length")
-
-   val strArray = Array(5) { charArrayOf() }
-
-    str.forEach { c ->
-        strArray[0] += patternP1
-        strArray[1] += patternP2
-        strArray[2] += "#.${c}.".toCharArray()
-        strArray[3] += patternP2
-        strArray[4] += patternP1
-    }
-    strArray[0] += charArrayOf('.')
-    strArray[1] += charArrayOf('.')
-    strArray[2] += charArrayOf('#')
-    strArray[3] += charArrayOf('.')
-    strArray[4] += charArrayOf('.')
-
-    (1..str.length/3).forEach { idx ->
-        strArray[0][idx*12-2] = '*'
-        strArray[1][idx*12-3] = '*'
-        strArray[1][idx*12-1] = '*'
-        strArray[2][idx*12-4] = '*'
-        strArray[2][idx*12] = '*'
-        strArray[3][idx*12-3] = '*'
-        strArray[3][idx*12-1] = '*'
-        strArray[4][idx*12-2] = '*'
+    var probabilityList = mutableListOf<List<Int>>()
+    (0..<missionNumber).forEach {
+        probabilityList += readln().split(" ").map { it.toInt() }
     }
 
-    strArray.forEach {
-        println(it)
+    probabilityList.forEach { probability ->
+        probability.forEach {
+            print("${it} ")
+        }
+        println()
     }
+
+
+
+
+
+
+
+
+
 }
